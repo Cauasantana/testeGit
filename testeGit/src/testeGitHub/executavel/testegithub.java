11474ce6633ee114f965e0aa1b1cdecc97a5e7b1
Cauã <Cau@DESKTOP-PT2JJOF>
2022-10-11 18:31:53 -0300

    teste

---
+++ testeGit/src/testeGitHub/executavel/testegithub.java
@@ -0,0 +1,25 @@
+package testeGitHub.executavel;
+
+public class testegithub {
+	
+	 
+		public static void main(String[] args) {
+			
+			int nota1 = 100;
+			int nota2 = 50;
+			int nota3 = 60;
+			int nota4 = 600;
+			
+			int mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+			
+			if(mediaFinal >= 60) {
+				System.out.println("Vc esta aprovado");
+			}else {
+				System.out.println("Vc esta reprovado");
+			}
+			
+		}
+		
+		
+	
+}
